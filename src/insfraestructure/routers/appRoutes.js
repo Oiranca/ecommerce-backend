03ce242e64b979f7463af98adc4e3fc961600a6b
Samuel Romero Arbelo <oiranca@gmail.com>
2021-01-01 20:54:52 +0000

    New prodcuts controller

--- src/insfraestructure/routers/appRoutes.js
+++ src/insfraestructure/routers/appRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import userController from '../controllers/users/userController';
+import productController from '../controllers/product/productController';
 
 const userRouters = express.Router();
 
 userRouters.post('/users-register', userController.registerUsers);
 userRouters.post('/admin-register', userController.registerUsers);
+userRouters.post('/products-register', productController.createProduct);
 
 //routers.get()
 
--- src/insfraestructure/routers/routers.js
+++ src/insfraestructure/routers/routers.js
@@ -3,4 +3,5 @@ import userRouters from './appRoutes';
 export default (app) => {
     app.use('/api/users', userRouters);
     app.use('/api/admin', userRouters);
+    app.use('/api/product', userRouters);
 };
