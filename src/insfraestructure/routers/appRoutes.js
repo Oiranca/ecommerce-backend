f12ca3e0d775e76b48f128bdc11c3659c76c6a48
Samuel Romero Arbelo <oiranca@gmail.com>
2021-01-01 21:18:13 +0000

    New store controller

--- src/insfraestructure/routers/appRoutes.js
+++ src/insfraestructure/routers/appRoutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import userController from '../controllers/users/userController';
 import productController from '../controllers/product/productController';
 import providerController from '../controllers/provider/providerController';
+import storeController from '../controllers/store/storeController';
 
 const userRouters = express.Router();
 
@@ -11,6 +12,7 @@ userRouters.post('/admin-register', userController.registerUsers);
 userRouters.post('/products-register', productController.createProduct);
 //Esta ruta es provisional ya que solamente el admin va poder crear proveedores
 userRouters.post('/admin/provider-register', providerController.providerRegister);
+userRouters.post('/store', storeController.registerInStore);
 
 //routers.get()
 
