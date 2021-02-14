b31c520ad2a007b9e4751fd29567ad32320c8245
Samuel <55623374+Oiranca@users.noreply.github.com>
2021-02-14 16:32:01 +0000

    Create product, create store and update stock (#11)
    * Fixed profile update
    
    * Refactor middlewares
    
    * Create middleware to create product
    
    * Create and delete stock into store

--- src/insfraestructure/controllers/product/productController.js
+++ src/insfraestructure/controllers/product/productController.js
@@ -1,21 +1,97 @@
 import Products from '../../../domine/model/products';
+import Store from '../../../domine/model/store';
+
+const findProduct = async (req) => {
+    return Products.findOne({ ean: req.body.ean }).select({
+        _id: 1,
+    });
+};
 
 const createProduct = async (req, res) => {
     try {
-        const { id_Provider, productName, ean, pvd } = req.body;
+        const { id_Provider, productName, ean, pvd, quantity } = req.body;
+        const existProduct = await findProduct(req);
 
-        await Products.create({
-            id_Provider,
-            productName,
-            ean,
-            pvd,
-        });
+        if (existProduct) {
+            await Store.findOne({
+                id_Product: existProduct._id,
+            })
+                .select({ _id: 1, stock: 1 })
+                .then(async (stocks) => {
+                    const sumStock = stocks.stock + parseInt(quantity);
+                    await Store.findOneAndUpdate(
+                        { _id: stocks._id },
+                        {
+                            stock: sumStock,
+                        },
+                    );
+                });
+        } else {
+            await Products.create({
+                id_Provider,
+                productName,
+                ean,
+                pvd,
+            }).then((product) => {
+                Store.create({
+                    id_Product: product._id,
+                    id_Provider: product.id_Provider,
+                    stock: quantity,
+                });
+            });
+        }
+
         res.send({ status: 'ok', message: 'Product created' });
     } catch (e) {
         res.status(500).send({ status: 'Error', message: e });
     }
 };
-
+const deleteProduct = async (req, res) => {
+    try {
+        const { quantity } = req.body;
+        const existProduct = await findProduct(req);
+        if (existProduct) {
+            await Store.findOne({
+                id_Product: existProduct._id,
+            })
+                .select({ _id: 1, stock: 1, id_Product: 1 })
+                .then(async (stocks) => {
+                    if (stocks.stock > 0 && stocks.stock >= quantity) {
+                        const deleteStock = stocks.stock - parseInt(quantity);
+                        const stockController = await Store.findOneAndUpdate(
+                            { _id: stocks._id },
+                            {
+                                $set: {
+                                    stock: deleteStock,
+                                },
+                            },
+                            { new: true },
+                        );
+                        res.send({ status: 'ok', message: 'Product deleted' });
+                        if (stockController.stock === 0) {
+                            await Store.findOneAndDelete({ _id: stocks._id })
+                                .select({
+                                    id_Product: 1,
+                                })
+                                .then(async (productToDeleted) => {
+                                    await Products.findByIdAndDelete({
+                                        _id: productToDeleted.id_Product,
+                                    });
+                                });
+                        }
+                    } else {
+                        res.status(500).send({
+                            status: 'Error',
+                            message: 'DELETED NOT POSSIBLE',
+                        });
+                    }
+                });
+        }
+    } catch (e) {
+        res.status(500).send({ status: 'Error', message: 'DELETED NOT POSSIBLE' });
+    }
+};
 export default {
     createProduct,
+    deleteProduct,
 };
--- src/insfraestructure/routers/appRoutes.js
+++ src/insfraestructure/routers/appRoutes.js
@@ -5,66 +5,77 @@ import providerController from '../controllers/provider/providerController';
 import storeController from '../controllers/store/storeController';
 import loginController from '../controllers/login/loginController';
 import profileController from '../controllers/profiles/profileController';
-import {
-    checkAuth,
-    isCorrectHost,
-    isAdmin,
-    existAsEmployee,
-    existAsAdmin,
-} from '../../domine/middlewares/auth';
-import * as path from 'path';
+import { middlewares } from '../../domine/middlewares/auth';
 
 const userRouters = express.Router();
 
 //Register Routes
 userRouters.post('/user/users-register', usersController.registerUsers);
-userRouters.post('/admin/admin-register', existAsEmployee, usersController.registerUsers);
+userRouters.post(
+    '/admin/admin-register',
+    middlewares.existAsEmployee,
+    usersController.registerUsers,
+);
 
 /*Sólo el administrador puede crear al empleado y al provider*/
 userRouters.post(
     '/employee/employee-register',
-    isCorrectHost,
-    isAdmin,
-    existAsAdmin,
+    middlewares.isCorrectHost,
+    middlewares.isAdmin,
+    middlewares.existAsAdmin,
     usersController.registerEmployee,
 );
 userRouters.post(
     '/admin/provider-register',
-    isCorrectHost,
-    isAdmin,
+    middlewares.isCorrectHost,
+    middlewares.isAdmin,
     providerController.providerRegister,
 );
 
-userRouters.post('/products-register', productController.createProduct);
+userRouters.post(
+    '/products-register',
+    middlewares.crudProduct,
+    productController.createProduct,
+);
+userRouters.post(
+    '/deleted-product',
+    middlewares.crudProduct,
+    productController.deleteProduct,
+);
 userRouters.post('/store', storeController.registerInStore);
 
 //Login Routes
-userRouters.post('/login', isCorrectHost, loginController.login);
-userRouters.post('/company/login', isCorrectHost, loginController.loginAdmin);
+userRouters.post('/login', middlewares.isCorrectHost, loginController.login);
+userRouters.post('/company/login', middlewares.isCorrectHost, loginController.loginAdmin);
 
 // Profile Routes
 userRouters.post(
     '/admin/employee-profile',
-    isCorrectHost,
-    checkAuth,
-    isAdmin,
+    middlewares.isCorrectHost,
+    middlewares.checkAuth,
+    middlewares.isAdmin,
     profileController.findEmployeeProfile,
 );
 
-userRouters.get('/profile', isCorrectHost, checkAuth, profileController.findUsersProfile);
+userRouters.get(
+    '/profile',
+    middlewares.isCorrectHost,
+    middlewares.checkAuth,
+    profileController.findUsersProfile,
+);
 
 userRouters.get(
     '/employee/profile',
-    isCorrectHost,
-    checkAuth,
+    middlewares.isCorrectHost,
+    middlewares.checkAuth,
     profileController.findEmployeeProfile,
 );
 
 userRouters.get(
     '/admin/profile',
-    isCorrectHost,
-    checkAuth,
-    isAdmin,
+    middlewares.isCorrectHost,
+    middlewares.checkAuth,
+    middlewares.isAdmin,
     profileController.findAdminProfile,
 );
 
@@ -72,23 +83,23 @@ userRouters.get(
 
 userRouters.post(
     '/admin/update-profile',
-    isCorrectHost,
-    checkAuth,
-    isAdmin,
+    middlewares.isCorrectHost,
+    middlewares.checkAuth,
+    middlewares.isAdmin,
     profileController.usersUpdateProfile,
 );
 
 userRouters.post(
     '/update-profile',
-    isCorrectHost,
-    checkAuth,
+    middlewares.isCorrectHost,
+    middlewares.checkAuth,
     profileController.usersUpdateProfile,
 );
 
 userRouters.post(
     '/employee/update-profile',
-    isCorrectHost,
-    checkAuth,
+    middlewares.isCorrectHost,
+    middlewares.checkAuth,
     profileController.usersUpdateProfile,
 );
 
