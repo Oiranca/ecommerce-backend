78c18a6c6c377ae88d61d32bc3af37f9b8356c17
Samuel <oiranca@gmail.com>
2021-02-14 14:38:10 +0000

    Refactor middlewares

--- src/domine/middlewares/auth.js
+++ src/domine/middlewares/auth.js
@@ -3,121 +3,119 @@ import Employee from '../model/employee';
 import Admin from '../model/admins';
 import roles from '../model/roles';
 
+const middleware = {
+    isCorrectHost: (req, res, next) => {
+        const validHost = req.hostname;
 
-const isCorrectHost = (req, res, next) => {
-    const validHost = req.hostname;
-
-    if (process.env.HOST === validHost) {
-        next();
-    } else {
-        res.status(404).send({ status: 'NOT FOUND' });
-    }
-};
-
-const checkAuth = (req, res, next) => {
-    try {
-        const tokenUser = req.headers['token-users'];
-        if (tokenUser) {
-            const { email } = jwt.verify(tokenUser, process.env.SECRET_TOKEN);
-            req.session = {
-                email,
-            };
+        if (process.env.HOST === validHost) {
             next();
         } else {
-            throw {
-                code: 403,
-                status: 'ACCESS_DENIED',
-                message: 'Missing header token',
-            };
+            res.status(404).send({ status: 'NOT FOUND' });
         }
-    } catch (e) {
-        res.send({ status: '401', message: e.message });
-    }
-};
-
-const isAdmin = (req, res, next) => {
-    try {
-        const tokenUser = req.headers['token-users'];
-        if (tokenUser) {
-            const { role } = jwt.verify(tokenUser, process.env.SECRET_TOKEN);
-            req.session = {
-                role,
-            };
-            if (role === roles.admin) {
+    },
+    checkAuth: (req, res, next) => {
+        try {
+            const tokenUser = req.headers['token-users'];
+            if (tokenUser) {
+                const { email } = jwt.verify(tokenUser, process.env.SECRET_TOKEN);
+                req.session = {
+                    email,
+                };
                 next();
             } else {
                 throw {
                     code: 403,
                     status: 'ACCESS_DENIED',
-                    message: 'NOT CORRECT ROLE',
+                    message: 'Missing header token',
                 };
             }
-        } else {
-            throw {
-                code: 403,
-                status: 'ACCESS_DENIED',
-                message: 'NOT CORRECT TOKEN',
-            };
+        } catch (e) {
+            res.send({ status: '401', message: e.message });
         }
-    } catch (e) {
-        res.send({ status: 403, message: e.message });
-    }
-};
-const existAsEmployee = async (req, res, next) => {
-    try {
-        const { email, identification } = req.body;
-        if (email) {
-            const findEmailExist = await Employee.findOne({ email: email });
-            const findIdentificationExist = await Employee.findOne({
-                identification: identification,
-            });
-            if (!findEmailExist && !findIdentificationExist) {
-                next();
+    },
+    isAdmin: (req, res, next) => {
+        try {
+            const tokenUser = req.headers['token-users'];
+            if (tokenUser) {
+                const { role } = jwt.verify(tokenUser, process.env.SECRET_TOKEN);
+                req.session = {
+                    role,
+                };
+                if (role === roles.admin) {
+                    next();
+                } else {
+                    throw {
+                        code: 403,
+                        status: 'ACCESS_DENIED',
+                        message: 'NOT CORRECT ROLE',
+                    };
+                }
             } else {
                 throw {
                     code: 403,
                     status: 'ACCESS_DENIED',
-                    message: 'EXIST INTO DATABASE',
+                    message: 'NOT CORRECT TOKEN',
                 };
             }
-        } else {
-            throw {
-                code: 403,
-                status: 'ACCESS_DENIED',
-                message: 'NOT CORRECT TOKEN',
-            };
+        } catch (e) {
+            res.send({ status: 403, message: e.message });
         }
-    } catch (e) {
-        res.send({ status: 403, message: e.message });
-    }
-};
-const existAsAdmin = async (req, res, next) => {
-    try {
-        const { email, identification } = req.body;
-        if (email) {
-            const findEmailExist = await Admin.findOne({ email: email });
-            const findIdentificationExist = await Admin.findOne({
-                identification: identification,
-            });
-            if (!findEmailExist && !findIdentificationExist) {
-                next();
+    },
+    existAsEmployee: async (req, res, next) => {
+        try {
+            const { email, identification } = req.body;
+            if (email) {
+                const findEmailExist = await Employee.findOne({ email: email });
+                const findIdentificationExist = await Employee.findOne({
+                    identification: identification,
+                });
+                if (!findEmailExist && !findIdentificationExist) {
+                    next();
+                } else {
+                    throw {
+                        code: 403,
+                        status: 'ACCESS_DENIED',
+                        message: 'EXIST INTO DATABASE',
+                    };
+                }
             } else {
                 throw {
                     code: 403,
                     status: 'ACCESS_DENIED',
-                    message: 'EXIST INTO DATABASE',
+                    message: 'NOT CORRECT TOKEN',
                 };
             }
-        } else {
-            throw {
-                code: 403,
-                status: 'ACCESS_DENIED',
-                message: 'NOT CORRECT TOKEN',
-            };
+        } catch (e) {
+            res.send({ status: 403, message: e.message });
         }
-    } catch (e) {
-        res.send({ status: 403, message: e.message });
-    }
+    },
+    existAsAdmin: async (req, res, next) => {
+        try {
+            const { email, identification } = req.body;
+            if (email) {
+                const findEmailExist = await Admin.findOne({ email: email });
+                const findIdentificationExist = await Admin.findOne({
+                    identification: identification,
+                });
+                if (!findEmailExist && !findIdentificationExist) {
+                    next();
+                } else {
+                    throw {
+                        code: 403,
+                        status: 'ACCESS_DENIED',
+                        message: 'EXIST INTO DATABASE',
+                    };
+                }
+            } else {
+                throw {
+                    code: 403,
+                    status: 'ACCESS_DENIED',
+                    message: 'NOT CORRECT TOKEN',
+                };
+            }
+        } catch (e) {
+            res.send({ status: 403, message: e.message });
+        }
+    },
 };
-
-export { checkAuth, isCorrectHost, isAdmin, existAsEmployee, existAsAdmin };
+export const middlewares = middleware;
