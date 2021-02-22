a3c1bac6e988c25de10300673061e3485c3900cd
Samuel <oiranca@gmail.com>
2021-02-22 13:56:08 +0000

    Modify quantity and total into basket

--- src/insfraestructure/controllers/basket/basketController.js
+++ src/insfraestructure/controllers/basket/basketController.js
@@ -15,50 +15,127 @@ const insertProductsIntoBasket = async (
     res,
     userCredential,
 ) => {
-    let idShopOnline;
-    let basketProduct;
-    let totalActive;
-    let quantityActive;
+    let idShopOnline = '';
+    let basketProduct = '';
+    let totalActive = '';
+    let quantityActive = '';
+    let idProductActive = '';
+    let findProductActive = '';
+    const findIntoBasketActive = (shopCredential) => {
+        findProductActive = shopCredential.basket_products.filter(
+            (itemsActive) => itemsActive.id_product === productToSell._id.toString(),
+        );
+        idShopOnline = shopCredential._id;
+        idProductActive = findProductActive.map((idIntoBasket) => idIntoBasket._id);
+        basketProduct = shopCredential.basket_products;
+        totalActive = shopCredential.total;
+        quantityActive = findProductActive.map((itemsIntoBasket) => {
+            if (itemsIntoBasket.id_product === productToSell._id.toString()) {
+                return Number(itemsIntoBasket.quantity);
+            }
+        });
+        findProductActive.map((itemsToModify) => {
+            if (itemsToModify.id_product === productToSell._id.toString()) {
+                for (let index in basketProduct) {
+                    if (index) {
+                        if (
+                            basketProduct[index].id_product ===
+                            productToSell._id.toString()
+                        ) {
+                            basketProduct[index].quantity =
+                                Number(quantityActive) + quantity;
+                            return basketProduct;
+                        }
+                    }
+                }
+            }
+        });
+        return {
+            findProductActive,
+            idShopOnline,
+            idProductActive,
+            basketProduct,
+            totalActive,
+            quantityActive,
+        };
+    };
     if (typeOfID === 'ONLINE') {
         for (let shopCredential of sellActive) {
             if (shopCredential.id_employee === 'ONLINE') {
-                idShopOnline = shopCredential._id;
-                basketProduct = shopCredential.basket_products;
-                totalActive = shopCredential.total;
-                quantityActive = shopCredential.basket_products.map(
-                    (itemsIntoBasket) => itemsIntoBasket.quantity,
-                );
+                findIntoBasketActive(shopCredential);
             }
         }
     } else {
         for (let shopCredential of sellActive) {
             if (shopCredential.id_employee === typeOfID._id.toString()) {
-                idShopOnline = shopCredential._id;
-                basketProduct = shopCredential.basket_products;
-                totalActive = shopCredential.total;
-                quantityActive = shopCredential.basket_products.map(
-                    (itemsIntoBasket) => itemsIntoBasket.quantity,
-                );
+                findIntoBasketActive(shopCredential);
             }
         }
     }
 
     if (idShopOnline) {
-        const productExist = await Basket.findOneAndUpdate(
-            {
-                'basket_products.id_product': productToSell._id.toString(),
-            },
-            {
-                'basket_products.$.quantity': Number(quantityActive) + quantity,
+        if (idProductActive.toString()) {
+            const a = await Basket.findOneAndUpdate(
+                {
+                    _id: idShopOnline,
+                },
+                {
+                    $set: {
+                        basket_products: basketProduct,
+                    },
+                    total:
+                        totalActive +
+                        (productToSell.pvd * taxes.IGIC + productToSell.pvd) * quantity,
+                },
+            ).select({
+                _id: 0,
+                'basket_products.pvp': 1,
+                'basket_products.quantity': 1,
+            });
 
-                total:
-                    totalActive +
-                    (productToSell.pvd * taxes.IGIC + productToSell.pvd) * quantity,
-            },
-        ).select({ _id: 0, 'basket_products.pvp': 1, 'basket_products.quantity': 1 });
+            res.send({ status: 'Ok', message: 'PRODUCT INTRODUCED' });
+        } else {
+            await Basket.findByIdAndUpdate(
+                { _id: idShopOnline._id },
+                {
+                    $set: {
+                        basket_products: [
+                            ...basketProduct,
+                            {
+                                id_product: productToSell._id,
+                                quantity: quantity,
+                                pvp: productToSell.pvd * taxes.IGIC + productToSell.pvd,
+                            },
+                        ],
+                    },
+                    total:
+                        totalActive +
+                        (productToSell.pvd * taxes.IGIC + productToSell.pvd) * quantity,
+                },
+            );
+            res.send({ status: 'Ok', message: 'PRODUCT INTRODUCED' });
+        }
+        /*{
+			'basket_products.$.quantity': Number(quantityActive) + quantity,
 
-        res.send({ status: 'Ok', message: 'PRODUCT INTRODUCED' });
-
+			total:
+				totalActive +
+				(productToSell.pvd * taxes.IGIC + productToSell.pvd) * quantity,
+		},*/
+        // const productExist = await Basket.findOneAndUpdate(
+        //     {
+        //         'basket_products.id_product': productToSell._id.toString(),
+        //     },
+        //     {
+        //         'basket_products.$.quantity': Number(quantityActive) + quantity,
+        //
+        //         total:
+        //             totalActive +
+        //             (productToSell.pvd * taxes.IGIC + productToSell.pvd) * quantity,
+        //     },
+        // ).select({ _id: 0, 'basket_products.pvp': 1, 'basket_products.quantity': 1 });
+        //
+        // res.send({ status: 'Ok', message: 'PRODUCT INTRODUCED' });
         // await Basket.findByIdAndUpdate(
         //     { _id: idShopOnline._id },
         //     {
